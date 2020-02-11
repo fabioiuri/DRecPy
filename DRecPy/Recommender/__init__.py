f1ab89515724352aaf685f94cf43e13318350a16
Fabio <fabioiuri@live.com>
2020-02-11 23:03:46 +0000

    Remove old test recommender from the Recommender __init__ file

--- DRecPy/Recommender/__init__.py
+++ DRecPy/Recommender/__init__.py
@@ -1,11 +1,9 @@
 from .recommender_abc import RecommenderABC
 from .dmf import DMF
 from .cdae import CDAE
-from .test import Test
 
 __all__ = [
     'RecommenderABC',
     'DMF',
-    'CDAE',
-    'Test'
+    'CDAE'
 ]
