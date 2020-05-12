4d24535776ee8dd2c0f9a26dce4734cd399894ae
Fabio <fabioiuri@live.com>
2020-05-12 21:56:40 +0100

    Update splitting example to include the total split duration

--- examples/splitting.py
+++ examples/splitting.py
@@ -1,5 +1,6 @@
 from DRecPy.Evaluation import leave_k_out
 from DRecPy.Dataset import get_full_dataset
+import time
 
 dataset = get_full_dataset("ml-100k")
 print('Full dataset', dataset)
@@ -8,7 +9,8 @@ print('Full dataset', dataset)
 # If a given user does not have k interactions, all interactions stay on train set.
 # Although, if a given user has < min_user_interactions, it will be removed
 # from both sets.
+start_t = time.time()
 dataset_train, dataset_test = leave_k_out(dataset, k=10, min_user_interactions=20)
+print(f'Splitting complete. Took: {time.time() - start_t}s')
 print('Train dataset', dataset_train)
 print('Test dataset', dataset_test)
-
