7aca8dbd8230b74db75fdf04587d56eb2540d7c1
Fabio <fabioiuri@live.com>
2020-05-12 21:44:36 +0100

    Add cdae training and evaluation figures

--- examples/cdae.py
+++ examples/cdae.py
@@ -2,16 +2,16 @@ from DRecPy.Recommender import CDAE
 from DRecPy.Dataset import get_train_dataset
 from DRecPy.Dataset import get_test_dataset
 from DRecPy.Evaluation import ranking_evaluation
-from DRecPy.Evaluation import predictive_evaluation
 import time
 
 ds_train = get_train_dataset('ml-100k')
 ds_test = get_test_dataset('ml-100k')
 
 start_train = time.time()
-cdae = CDAE(min_interaction=0, seed=10)
-cdae.fit(ds_train, epochs=50)
+cdae = CDAE(hidden_factors=50, corruption_level=0.2, loss='bce', seed=10)
+cdae.fit(ds_train, learning_rate=0.001, reg_rate=0.0, epochs=50, batch_size=64, neg_ratio=5)
 print("Training took", time.time() - start_train)
 
-print(ranking_evaluation(cdae, ds_test, n_test_users=100, seed=10))
-print(predictive_evaluation(cdae, ds_test, skip_errors=True))
+print(ranking_evaluation(cdae, ds_test, k=[1, 5, 10], novelty=True, n_test_users=100, n_pos_interactions=1,
+                         n_neg_interactions=100, generate_negative_pairs=True, seed=10, max_concurrent_threads=4,
+                         verbose=True))
--- examples/cdae_validation.py
+++ examples/cdae_validation.py
@@ -2,32 +2,30 @@ from DRecPy.Recommender import CDAE
 from DRecPy.Dataset import get_train_dataset
 from DRecPy.Dataset import get_test_dataset
 from DRecPy.Evaluation import ranking_evaluation
-from DRecPy.Evaluation import predictive_evaluation
 from DRecPy.Evaluation import leave_k_out
+from DRecPy.Evaluation.Metrics import ndcg
+from DRecPy.Evaluation.Metrics import hit_ratio
 import time
 
 
 ds_train = get_train_dataset('ml-100k')
 ds_test = get_test_dataset('ml-100k')
-ds_train, ds_val = leave_k_out(ds_train, k=5, min_user_interactions=10)
+ds_train, ds_val = leave_k_out(ds_train, k=1, min_user_interactions=10)
 
 
 def epoch_callback_fn(model):
-    train_res = {'train_' + k: v for k, v in
-                 ranking_evaluation(model, n_pos_interactions=2, n_neg_interactions=19, n_test_users=50, verbose=False, k=10).items()
-                 if 'HR' in k}
-    val_res = {'val_' + k: v for k, v in
-               ranking_evaluation(model, ds_val, n_pos_interactions=2, n_neg_interactions=19, n_test_users=50, verbose=False, k=10).items()
-               if 'HR' in k}
-
-    return dict(train_res, **val_res)
+    return {'val_' + metric: v for metric, v in
+            ranking_evaluation(model, ds_val, n_pos_interactions=1, n_neg_interactions=100,
+                               generate_negative_pairs=True, k=10, verbose=False, seed=10,
+                               metrics={'HR': (ndcg, {}), 'NDCG': (hit_ratio, {})}).items()}
 
 
 start_train = time.time()
-cdae = CDAE(min_interaction=0, seed=10)
-cdae.fit(ds_train, epochs=20, learning_rate=0.0001, reg_rate=0.001,
-         epoch_callback_fn=epoch_callback_fn, epoch_callback_freq=1)
+cdae = CDAE(hidden_factors=50, corruption_level=0.2, loss='bce', seed=10)
+cdae.fit(ds_train, learning_rate=0.001, reg_rate=0.001, epochs=80, batch_size=64, neg_ratio=5,
+         epoch_callback_fn=epoch_callback_fn, epoch_callback_freq=20)
 print("Training took", time.time() - start_train)
 
-print(ranking_evaluation(cdae, ds_test, n_test_users=100, seed=10))
-print(predictive_evaluation(cdae, ds_test, skip_errors=True))
+print(ranking_evaluation(cdae, ds_test, k=[1, 5, 10], novelty=True, n_pos_interactions=1,
+                         n_neg_interactions=100, generate_negative_pairs=True, seed=10,
+                         max_concurrent_threads=4, verbose=True))
