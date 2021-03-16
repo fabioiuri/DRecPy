045e69b084ba5c30b5cf66669ce28f902e12daa8
Fabio <fabioiuri@live.com>
2021-03-16 20:48:38 +0000

    Update setup.py version to 0.2.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("requirements.txt", "r") as fh:
 
 setup(
     name="DRecPy",
-    version="0.2.1",
+    version="0.2.2",
     author="Fabio Colaço",
     author_email="fabioiuri@live.com",
     description="Deep Recommenders with Python: "
